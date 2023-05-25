8a4ee658da48ec0ca7c49303998989b0b4c96158
peosoow <ws71144712@daum.net>
2023-05-25 11:43:50 +0900

    HomeInfo 수정

--- src/page/Home/HomeInfo.vue
+++ src/page/Home/HomeInfo.vue
@@ -4,8 +4,8 @@
       <div class="w-full mx-auto">
         <h2 class="mb-10 ml-[1%] text-3xl">여행정보</h2>
         <div class="flex justify-items-center border border-black mb-10 text-center text-xl rounded-lg py-4 ">
-          <p class="basis-[49%] cursor-pointer">인기</p>
-          <p class="basis-[49%] relative after:absolute after:w-[1px] after:h-[32px] after:bg-gray-900 after:left-0 after:top-2/4 after:-translate-y-2/4 cursor-pointer">지역별</p>
+          <p class="basis-[49%] cursor-pointer"><router-link to="/tour/info" class="block h-full">대표 여행지</router-link></p>
+          <p class="basis-[49%] relative after:absolute after:w-[1px] after:h-[32px] after:bg-gray-900 after:left-0 after:top-2/4 after:-translate-y-2/4 cursor-pointer"><router-link to="/tour/course" class="block h-full">지역 추천 코스</router-link></p>
         </div>
         <div class="w-full flex justify-center text-xl py-4 gap-x-[5%]">
           <p v-for="e in Tab" :key="e" class="border border-black/30 py-[1%] px-[7%] rounded-xl cursor-pointer hover:bg-black hover:text-white shrink-0">{{ e }}</p>
@@ -53,7 +53,8 @@ export default {
       // (12: 관광지, 14: 문화시설, 15: 축제공연행사, 25: 여행코스, 28: 레포츠, 32: 숙박, 38: 쇼핑, 39: 음식점)
       selectContentTypeCode: '15',
       selectContentTypeName: '축제공연행사',
-      selectArrange: 'O', // 정렬구분 (A=제목순, C=수정일순, D=생성일순) 대표이미지가반드시있는정렬(O=제목순, Q=수정일순, R=생성일순)
+      arrange: 'R', 
+      selectArrange: 'R', // 정렬구분 (A=제목순, C=수정일순, D=생성일순) 대표이미지가반드시있는정렬(O=제목순, Q=수정일순, R=생성일순)
       /* 공통 */
       Mobileos: 'ETC', // OS 구분 : IOS (아이폰), AND (안드로이드), WIN (윈도우폰), ETC(기타)
       contentTypeId: '', // 관광타입(12:관광지, 14:문화시설, 15:축제공연행사, 25:여행코스, 28:레포츠, 32:숙박, 38:쇼핑, 39:음식점)
@@ -81,13 +82,12 @@ export default {
       // 세부 api별 옵션 parameter
       const contentTypeInfo = '&contentTypeId=' + this.selectContentTypeCode
       const areaCodeInfo = '&areaCode=' + this.selectAreaCode
-      
+      const arrangeInfo = '&arrange=' + this.selectArrange      
       // 총 개수 가져오기
       // 조건문으로 아래 페이지 번호를 눌렀을 때는 코드 실행 안하도록 함
       
       let travelInfo;
-      travelInfo = axios.get(commonUrl + contentTypeInfo + areaCodeInfo + `&numOfRows=5`)
-      console.log(commonUrl + contentTypeInfo + areaCodeInfo + `&numOfRows=5`)
+      travelInfo = axios.get(commonUrl + contentTypeInfo + areaCodeInfo + arrangeInfo + `&numOfRows=5`)
       travelInfo.then(
         (res) => {
           console.log(res.data.response.body)
@@ -98,9 +98,6 @@ export default {
         console.log(err)
       })
     }
-  },
-  created(){
-
   },
 }
 </script>
